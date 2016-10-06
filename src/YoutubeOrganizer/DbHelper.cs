37085216f9cdcd6daab2fd34dbb03861feda7a19
Zoltanar <universegusty@gmail.com>
2016-10-06 01:43:08 +0100

    Added Pagination to Videos Page.

--- src/YoutubeOrganizer/DbHelper.cs
+++ src/YoutubeOrganizer/DbHelper.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using YoutubeOrganizer.Data;
 using YoutubeOrganizer.Models;
